edb086e757b861d12d48487527a7d6bd38eae040
hoonloper <yonghoonj98@gmail.com>
2024-06-02 15:27:25 +0900

    Test: PostService를 FakePostRepository를 이용하여 테스트

--- test-code-with-architecture/src/test/java/com/example/demo/post/domain/PostTest.java
+++ test-code-with-architecture/src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,16 +1,15 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.mock.TestClockHolder;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
-
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PostTest {
   @Test
-  public void PostCreate으로_게시물을_만들_수_있다() {
+  public void PostCreate로_게시물을_만들_수_있다() {
     // given
     PostCreate postCreate = PostCreate.builder()
       .writerId(1)
@@ -25,14 +24,43 @@ public class PostTest {
       .build();
 
     // when
-    Post post = Post.from(writer, postCreate);
+    Post post = Post.from(writer, postCreate, new TestClockHolder(1678530679999L));
 
     // then
     assertThat(post.getContent()).isEqualTo("hello");
+    assertThat(post.getCreatedAt()).isEqualTo(1678530679999L);
     assertThat(post.getWriter().getEmail()).isEqualTo("test@test.com");
     assertThat(post.getWriter().getNickname()).isEqualTo("test");
     assertThat(post.getWriter().getAddress()).isEqualTo("Busan");
     assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
     assertThat(post.getWriter().getCertificationCode()).isEqualTo("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+  }
+
+  @Test
+  public void PostUpdate로_게시물을_수정할_수_있다() {
+    // given
+    PostUpdate postUpdate = PostUpdate.builder()
+      .content("hello")
+      .build();
+    // given
+    User writer = User.builder()
+      .email("test@test.com")
+      .address("Busan")
+      .nickname("test")
+      .status(UserStatus.ACTIVE)
+      .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
+      .build();
+    Post post = Post.builder()
+      .writer(writer)
+      .content("hello")
+      .createdAt(1678530679908L)
+      .build();
+
+    // when
+    post = post.update(postUpdate, new TestClockHolder(1678530679999L));
+
+    // then
+    assertThat(post.getContent()).isEqualTo("hello");
+    assertThat(post.getModifiedAt()).isEqualTo(1678530679999L);
   }
 }
---
+++ test-code-with-architecture/src/test/java/com/example/demo/post/service/PostServiceTest.java
@@ -0,0 +1,106 @@
+package com.example.demo.post.service;
+
+import com.example.demo.mock.*;
+import com.example.demo.post.domain.Post;
+import com.example.demo.post.domain.PostCreate;
+import com.example.demo.post.domain.PostUpdate;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PostServiceTest {
+  private PostService postService;
+
+  @BeforeEach
+  void init() {
+    FakePostRepository fakePostRepository = new FakePostRepository();
+    FakeUserRepository fakeUserRepository = new FakeUserRepository();
+
+    this.postService = PostService.builder()
+      .postRepository(fakePostRepository)
+      .userRepository(fakeUserRepository)
+      .clockHolder(new TestClockHolder(1678530673958L))
+      .build();
+
+    User writer =
+      User.builder()
+        .id(1L)
+        .email("test@test.com")
+        .nickname("test")
+        .address("Seoul")
+        .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
+        .status(UserStatus.ACTIVE)
+        .lastLoginAt(0L)
+        .build();
+    User writer2 =
+      User.builder()
+        .id(2L)
+        .email("test2@test.com")
+        .nickname("test2")
+        .address("Busan")
+        .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+        .status(UserStatus.PENDING)
+        .lastLoginAt(0L)
+        .build();
+
+    fakePostRepository.save(
+      Post.builder()
+        .id(1L)
+        .content("test1")
+        .createdAt(1678530673958L)
+        .modifiedAt(0L)
+        .writer(writer)
+        .build()
+    );
+    fakeUserRepository.save(writer);
+    fakeUserRepository.save(writer2);
+  }
+  @Test
+  void getById_는_존재하는_게시물을_내려준다() {
+    // given
+    // when
+    Post result = postService.getById(1);
+
+    // then
+    assertThat(result.getContent()).isEqualTo("test1");
+    assertThat(result.getWriter().getEmail()).isEqualTo("test@test.com");
+  }
+
+  @Test
+  void postCreate_를_이용하여_게시물을_생성할_수_있다() {
+    // given
+    PostCreate postCreate = PostCreate.builder()
+      .writerId(1)
+      .content("testtest")
+      .build();
+
+    // when
+    Post result = postService.create(postCreate);
+
+    // then
+    assertThat(result.getId()).isNotNull();
+    assertThat(result.getContent()).isEqualTo("testtest");
+    assertThat(result.getCreatedAt()).isEqualTo(1678530673958L);
+    assertThat(result.getWriter().getCertificationCode()).isEqualTo("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+  }
+
+  @Test
+  void postUpdate_를_이용하여_게시물을_수정할_수_있다() {
+    // given
+    PostUpdate postUpdate = PostUpdate.builder()
+      .content("test!")
+      .build();
+
+    // when
+    postService.update(1, postUpdate);
+
+    // then
+    Post postEntity = postService.getById(1);
+    assertThat(postEntity.getId()).isNotNull();
+    assertThat(postEntity.getContent()).isEqualTo("test!");
+    assertThat(postEntity.getModifiedAt()).isEqualTo(1678530673958L);
+  }
+}
